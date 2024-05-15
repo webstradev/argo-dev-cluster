4f26a1a1b2863bfe9b3b93d94b26d54002a525bc
Erik Westra <e.s.westra.95@gmail.com>
2024-05-15 22:30:35 +0200

    dont use temp folder
    Signed-off-by: Erik Westra <e.s.westra.95@gmail.com>

--- setup-cluster.sh
+++ setup-cluster.sh
@@ -4,14 +4,8 @@ kubectl create ns argocd
 # Create the secret for the repository (needed for argo-cd)
 ./create-repo-secret.sh
 
-# Create a temporary directory
-tmpdir=$(mktemp -d)
-
-# Install argo-cd using the manifest in the temporary directory
-kubectl kustomize kustom/argo-cd/overlays --enable-helm --output $tmpdir/argo-cd.yaml
-kubectl apply -f $tmpdir/argo-cd.yaml
-
-rm -rf $tmpdir
+# Install argo=cd using the manifest
+kubectl kustomize kustom/argo-cd/overlays --enable-helm | kubectl apply -f -
 
 # Install the apps (this includes the argo-cd app)
 # That way it manages it self 
