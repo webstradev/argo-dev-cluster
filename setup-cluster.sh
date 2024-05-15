9017a0276eed819c9e716f60ee4582ef2a771e48
Erik Westra <e.s.westra.95@gmail.com>
2024-05-15 22:12:34 +0200

    Install argocd with a command using kustomize manifests.
    Signed-off-by: Erik Westra <e.s.westra.95@gmail.com>

--- setup-cluster.sh
+++ setup-cluster.sh
@@ -4,3 +4,11 @@ kubectl create ns argocd
 # Create the secret for the repository (needed for argo-cd)
 ./create-repo-secret.sh
 
+# Create a temporary directory
+tmpdir=$(mktemp -d)
+
+# Install argo-cd using the manifest in the temporary directory
+kubectl kustomize kustom/argo-cd/overlays --enable-helm --output $tmpdir/argo-cd.yaml
+kubectl apply -f $tmpdir/argo-cd.yaml
+
+rm -rf $tmpdir
