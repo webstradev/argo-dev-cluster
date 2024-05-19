7d2fa0fcd9c7b3d30c877f00e12405e6657bfccc
Erik Westra <e.s.westra.95@gmail.com>
2024-05-19 11:46:42 +0200

    fix repo-secret
    Signed-off-by: Erik Westra <e.s.westra.95@gmail.com>

--- create-repo-secret.sh
+++ create-repo-secret.sh
@@ -1,20 +1,22 @@
 #!/bin/bash
 
 # Prompt for SSH key location, GitHub username, and repository name
-read -p "Enter the SSH key location (default: ~/.ssh/id_rsa): " SSH_KEY_LOCATION
-SSH_KEY_LOCATION=${SSH_KEY_LOCATION:-~/.ssh/id_rsa}
+read -p "Enter the SSH key location (default: ~/.ssh/argocd): " SSH_KEY_LOCATION
+SSH_KEY_LOCATION=${SSH_KEY_LOCATION:-~/.ssh/argocd}
 
 read -p "Enter your GitHub username (default: webstradev): " GITHUB_USERNAME
 GITHUB_USERNAME=${GITHUB_USERNAME:-'webstradev'}
-read -p "Enter your repository name: (default: argo-dev-cluster)" REPO_NAME
+
+read -p "Enter your repository name (default: argo-dev-cluster): " REPO_NAME
 REPO_NAME=${REPO_NAME:-'argo-dev-cluster'}
 
-# Base64 encode the SSH private key
-SSH_PRIVATE_KEY=$(cat $SSH_KEY_LOCATION | base64 | tr -d '\n')
+# Read the SSH private key
+SSH_PRIVATE_KEY=$(cat $SSH_KEY_LOCATION)
 
-# Create the Kubernetes secret with the necessary label
-kubectl create secret generic argocd-repo-secret \
+# Create the Kubernetes secret with the correct label and structure
+kubectl create secret generic argocd-repo-secret\
   --namespace argocd \
+  --from-literal=type=git \
   --from-literal=url=git@github.com:$GITHUB_USERNAME/$REPO_NAME.git \
-  --from-literal=sshPrivateKey=$SSH_PRIVATE_KEY \
+  --from-literal=sshPrivateKey="$SSH_PRIVATE_KEY" \
   --dry-run=client -o yaml | kubectl label --local -f - argocd.argoproj.io/secret-type=repository -o yaml | kubectl apply -f -
