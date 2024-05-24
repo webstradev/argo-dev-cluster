f9e64aaaa3d4e55bdf63924a9ea0d31d32c517c2
Erik Westra <e.s.westra.95@gmail.com>
2024-05-25 01:04:00 +0200

    update crds
    Signed-off-by: Erik Westra <e.s.westra.95@gmail.com>

--- kustom/cert-manager/base/download-crds.sh
+++ kustom/cert-manager/base/download-crds.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-curl -L https://github.com/jetstack/cert-manager/releases/download/v1.10.1/cert-manager.crds.yaml -o kustom/cert-manager/base/cert-manager-crds.yaml
+curl -L https://github.com/jetstack/cert-manager/releases/download/v1.14.5/cert-manager.crds.yaml -o kustom/cert-manager/base/cert-manager-crds.yaml
