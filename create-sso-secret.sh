e6ecee81a26a8634611aab95e5834a9259fca570
Erik Westra <e.s.westra.95@gmail.com>
2024-05-25 11:37:07 +0200

    Add organisation to sso secret setup
    Signed-off-by: Erik Westra <e.s.westra.95@gmail.com>

--- create-sso-secret.sh
+++ create-sso-secret.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 
-# Prompt for GitHub OAuth client ID and client secret
+# Prompt for GitHub OAuth client ID, client secret and org
 read -p "Enter your GitHub OAuth client ID: " CLIENT_ID
 read -p "Enter your GitHub OAuth client secret: " CLIENT_SECRET
+read -p "Enter your GitHub organization: " ORG
+
 
 # Create the Kubernetes secret with the correct name, structure, and label
 kubectl create secret generic argocd-sso-key \
   --namespace argocd \
+  --from-literal=dex.github.org="$ORG" \
   --from-literal=dex.github.clientID="$CLIENT_ID" \
   --from-literal=dex.github.clientSecret="$CLIENT_SECRET" \
   --dry-run=client -o yaml | kubectl label --local -f - app.kubernetes.io/part-of=argocd -o yaml | kubectl apply -f -
