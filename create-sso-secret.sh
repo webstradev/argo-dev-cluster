b2945745bf3cc0d5e7d3ed1835309d338ae7f7bb
Erik Westra <e.s.westra.95@gmail.com>
2024-05-25 10:08:58 +0200

    Fix connectors
    Signed-off-by: Erik Westra <e.s.westra.95@gmail.com>

--- create-sso-secret.sh
+++ create-sso-secret.sh
@@ -7,8 +7,8 @@ read -p "Enter your GitHub OAuth client secret: " CLIENT_SECRET
 # Create the Kubernetes secret with the correct name, structure, and label
 kubectl create secret generic argocd-sso-key \
   --namespace argocd \
-  --from-literal=dex.acme.clientID="$CLIENT_ID" \
-  --from-literal=dex.acme.clientSecret="$CLIENT_SECRET" \
+  --from-literal=dex.github.clientID="$CLIENT_ID" \
+  --from-literal=dex.github.clientSecret="$CLIENT_SECRET" \
   --dry-run=client -o yaml | kubectl label --local -f - app.kubernetes.io/part-of=argocd -o yaml | kubectl apply -f -
 
 echo "Kubernetes secret 'argocd-sso-key' created in namespace 'argocd'."
