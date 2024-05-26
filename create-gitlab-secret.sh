27a3618d903b29b9fca24881a6e80b6004c47435
Erik Westra <e.s.westra.95@gmail.com>
2024-05-26 13:07:56 +0200

    add script to create gitlab token secret
    Signed-off-by: Erik Westra <e.s.westra.95@gmail.com>

---
+++ create-gitlab-secret.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# Prompt for Gitlab token name and access_token 
+read -p "Enter the Gitlab token name (default: k3s-dev-cluster): " GITLAB_TOKEN_NAME 
+GITLAB_TOKEN_NAME=${GITLAB_TOKEN_NAME:-k3s-dev-cluster}
+read -p "Enter your Gitlab access token: " GITLAB_ACCESS_TOKEN
+
+# Create kubernetes secret
+kubectl create secret generic gitlab-token\
+  --namespace external-secrets \
+  --from-literal=token_name="$GITLAB_TOKEN_NAME" \
+  --from-literal=token="$GITLAB_ACCESS_TOKEN" \
+  --dry-run=client -o yaml | kubectl apply -f -
