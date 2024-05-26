2fafd18e0d1002463a2d3480da45b4b793c924ca
Erik Westra <e.s.westra.95@gmail.com>
2024-05-26 16:37:17 +0200

    remove logigc
    Signed-off-by: Erik Westra <e.s.westra.95@gmail.com>

--- setup-cluster.sh
+++ setup-cluster.sh
@@ -23,30 +23,6 @@ kubectl kustomize apps/argo-cd --enable-helm | kubectl apply -f -
 # (argocd is one of these apps so now argocd manages itself)
 kubectl apply -k apps
 
-# Wait for all applications in the argocd namespace to be healthy and synced
-echo "Waiting for Argo CD applications to be healthy and synced..."
-
-while true; do
-  all_healthy=true
-  for app in $(kubectl get applications.argoproj.io -n argocd -o jsonpath='{.items[*].metadata.name}'); do
-    health_status=$(kubectl get applications.argoproj.io $app -n argocd -o jsonpath='{.status.health.status}')
-    sync_status=$(kubectl get applications.argoproj.io $app -n argocd -o jsonpath='{.status.sync.status}')
-    if [ "$health_status" != "Healthy" ] || [ "$sync_status" != "Synced" ]; then
-      all_healthy=false
-      echo "Waiting for application $app to be healthy and synced..."
-    fi
-  done
-  if [ "$all_healthy" = true ]; then
-    break
-  fi
-  sleep 30
-  echo "===== Retrying... ====="
-done
-
-echo "All Argo CD applications are healthy and synced."
-
 # Remove the charts from your local machine (just some clean up)
 rm -rf apps/argo-cd/charts 
 rm -rf apps/external-secrets/charts
-
-echo '<a href="argo.cluster.webstra.dev">Click here to go to Argo CD Dashboard</a>'
