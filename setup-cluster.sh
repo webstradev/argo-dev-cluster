505f7095edfc9ffe129e446e9daa988d2bd7745e
Erik Westra <e.s.westra.95@gmail.com>
2024-05-26 16:04:43 +0200

    Add some comments
    Signed-off-by: Erik Westra <e.s.westra.95@gmail.com>

--- setup-cluster.sh
+++ setup-cluster.sh
@@ -1,6 +1,6 @@
 # bin/bash
-kubectl create ns argocd
-kubectl create ns external-secrets
+# Create required namespaces for argocd and external-secrets
+kubectl kustomize apps/namespaces | kubectl apply -f -
 
 # Create secret for gitlab access token
 ./create-gitlab-secret.sh
@@ -8,15 +8,17 @@ kubectl create ns external-secrets
 # Install external-secrets using manifest 
 kubectl kustomize apps/external-secrets --enable-helm | kubectl apply -f -
 
+# Install the cluster secret store
+# (this connects to gitlabs and initialises the store)
 kubectl apply -k apps/cluster-secret-store
 
 # Install argocd using the manifest
 kubectl kustomize apps/argo-cd --enable-helm | kubectl apply -f -
 
-# Install the apps (this includes the argo-cd app)
-# (now argocd manages itself)
+# Install the apps using argocd (this will also create repo and sso secrets) 
+# (argocd is one of these apps so now argocd manages itself)
 kubectl apply -k apps
 
-# Remove the charts from your local machine
-rm -rf apps/argo-cd/charts
+# Remove the charts from your local machine (just some clean up)
+rm -rf apps/argo-cd/charts 
 rm -rf apps/external-secrets/charts
